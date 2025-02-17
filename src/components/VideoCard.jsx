5ad4da339392b6e1aef1a6416faf7fbdf0a155d7
surafel meles <melessurafel413@gmail.com>
2025-02-18 01:31:48 +0300

    commit

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import "../styles/components/videocard.css"
-
+import imgOne from '../assets/image.png'
+import imgTow from '../assets/image copy.png'
 function VideoCard() {
   return (
     <div class=' video_card_container '>
       <div class="card_one video_card">
-        <img src="./public/assets/image.png" alt="" />
+        <img src={imgOne} alt="" />
         <p>surafel meles</p>
       </div>
       <div class="card_tow video_card">
-        <img src="./public/assets/image copy.png" alt="" />
+        <img src={imgTow} alt="" />
         <p>surafel meles</p>
       </div>
     </div>
