f4277510977aa51370a758980df62ce1944a7cee
surafel meles <melessurafel413@gmail.com>
2025-02-18 01:14:13 +0300

    commit

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -5,11 +5,11 @@ function VideoCard() {
   return (
     <div class=' video_card_container '>
       <div class="card_one video_card">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMUDp3aV5EOTHgkQp6AB782rSKOyhhkdkx8Q&s" alt="" />
+        <img src="./public/assets/image.png" alt="" />
         <p>surafel meles</p>
       </div>
       <div class="card_tow video_card">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMUDp3aV5EOTHgkQp6AB782rSKOyhhkdkx8Q&s" alt="" />
+        <img src="./public/assets/image copy.png" alt="" />
         <p>surafel meles</p>
       </div>
     </div>
